b3d5ebf398860a00fe7360e1ff9fddb5a29924af
rungchan2 <leeh09077@gmail.com>
2022-05-21 18:35:25 +0900

    common mudule separation

--- starbucks-master/js/main.js
+++ starbucks-master/js/main.js
@@ -1,19 +1,3 @@
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click', function() {
-  searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus',function () {
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder', '통합검색');
-})
-
-searchInputEl.addEventListener('blur',function () {
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder', '');
-})
 
 
 const badgeEl = document.querySelector('header .badges');
@@ -150,6 +134,5 @@ spyEls.forEach(function (spyEl) {
     .addTo(new ScrollMagic.Controller());
 });
 
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear();
+
 
